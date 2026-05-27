6f911a8841e908c559e206319339976d7e34b54a
Megha0707 <meghana.srinivas0707@gmail.com>
2026-05-27 14:54:35 +0530

    Use existing resource groups

--- infra/resource_group.tf
+++ infra/resource_group.tf
@@ -1,20 +1,13 @@
-resource "azurerm_resource_group" "dev" {
-  name     = local.dev_resource_group_name
-  location = var.location
-
-  tags = local.dev_tags
+data "azurerm_resource_group" "dev" {
+  name = local.dev_resource_group_name
 }
 
-resource "azurerm_resource_group" "prod" {
-  name     = local.prod_resource_group_name
-  location = var.location
-
-  tags = local.prod_tags
+data "azurerm_resource_group" "prod" {
+  name = local.prod_resource_group_name
 }
 
 resource "azurerm_resource_group" "hub" {
   name     = local.hub_resource_group_name
   location = var.location
-
-  tags = local.hub_tags
+  tags     = local.hub_tags
 }
