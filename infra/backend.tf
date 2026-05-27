c089f13055cf40594933e57bb783a63734ac801c
Megha0707 <meghana.srinivas0707@gmail.com>
2026-05-27 14:30:22 +0530

    Fix backend auth

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "Grouper"
-    storage_account_name = "groupertfstatepoc01"
-    container_name       = "terraform"
-    key                  = "dcs-apps.tfstate"
-    use_azuread_auth     = true
-  }
+  backend "azurerm" {}
 }
